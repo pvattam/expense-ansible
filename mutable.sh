041dd15b448aca175f960322802d41ebb5c10e2c
Pushpak Vattam <vattampushpak243@gmail.com>
2024-01-20 20:33:29 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- mutable.sh
+++ mutable.sh
@@ -3,3 +3,12 @@ APP_VERSION=$2
 COMPONENT=$3
 
 env
+
+# Find the servers(Ansible Dynamic Inventory)
+aws ec2 describe-instances --filters "Name=tag:Name,Values=${ENV}-${COMPONENT}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text >inv
+
+# Change the Parameter store having the app version.
+aws ssm put-parameter --name "${ENV}.${COMPONENT}.app_version" --value "${APP_VERSION}" --type "String" --overwrite
+
+# Run ansible push on the servers
+ansible-playbook -i inv mutable-deploy.yml -e component=${COMPONENT} -e env=${ENV} -e version=${APP_VERSION} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD}
